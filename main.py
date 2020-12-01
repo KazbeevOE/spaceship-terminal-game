80548de8170817e5f2d71fb6f95223329926bac5
KazbeevOE <kazbeev@gmail.com>
2020-12-01 06:33:39 +0000

    Add generate_star_parameters func.

--- main.py
+++ main.py
@@ -1,16 +1,40 @@
 import asyncio
 import curses
 import time
+import random
 
 
 TIC_TIMEOUT = 0.1
+STAR_SHAPES = '+*.:'
 
+def draw(canvas):
+  window_height, window_width = canvas.getmaxyx()
+  max_amount_of_stars = random.randint(50, 100)
+  border_size = 1
 
-async def sleep(tics):
-  iteration_count = int(tics * 10)
+  canvas.border()
+  curses.curs_set(False)
+  canvas.refresh()
 
-  for _ in range(iteration_count):
-    await asyncio.sleep(0)
+  coroutines = []
+
+  for s in range(max_amount_of_stars):
+    row, column, symbol = generate_star_parametres(window_height, window_width, border_size)
+    coroutine = blink(canvas, row, column, symbol)
+    coroutines.append(coroutine)
+
+  while True:
+    for coroutine in coroutines:
+      coroutine.send(None)
+
+    time.sleep(0.1)
+    canvas.refresh()
+
+def generate_star_parametres(window_height, window_width, border_size):
+  row = random.randint(1, window_height - border_size - 1)
+  column = random.randint(1, window_width - border_size - 1)
+  symbol = random.choice(STAR_SHAPES)
+  return (row, column, symbol)
 
 async def blink(canvas, row, column, symbol='*'):
     while True:
@@ -26,27 +50,12 @@ async def blink(canvas, row, column, symbol='*'):
       canvas.addstr(row, column, symbol)
       await sleep(0.3)
 
+async def sleep(tics):
+  iteration_count = int(tics * 10)
 
-def draw(canvas):
-  canvas.border()
-  curses.curs_set(False)
-
-  coroutine_1 = blink(canvas, 1, 1, '*')
-  coroutine_2 = blink(canvas, 1, 2, '*')
-  coroutine_3 = blink(canvas, 1, 3, '*')
-  coroutine_4 = blink(canvas, 1, 4, '*')
-  coroutine_5 = blink(canvas, 1, 5, '*')
-  coroutines = [coroutine_1, coroutine_2, coroutine_3,
-                coroutine_4, coroutine_5]
-
-  while True:
-    for coroutine in coroutines:
-      coroutine.send(None)
-
-    time.sleep(0.1)
-    canvas.refresh()
-
-      
+  for _ in range(iteration_count):
+    await asyncio.sleep(0)
+ 
 if __name__ == '__main__':
     curses.update_lines_cols()
     curses.wrapper(draw)
